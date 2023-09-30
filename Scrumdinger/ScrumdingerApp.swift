dbda235c8e7b48ad01a6ef12dcb370e7713f9578
benjgrad <12bjg1@queensu.ca>
2023-09-30 15:25:33 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by bengrady on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
