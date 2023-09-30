7c6a5380c2898c5ff48992f27e2201cf70279488
benjgrad <12bjg1@queensu.ca>
2023-09-30 15:36:01 -0400

    Section 4 Supplement accessibility data

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -24,6 +24,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
                 .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
@@ -35,6 +38,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }.padding()
     }
